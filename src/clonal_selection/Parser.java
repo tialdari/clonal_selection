97fa25b0951008957a7e1b1c60b59c9c73fe424a
Asia <asialuk98@gmail.com>
2018-05-25 12:14:30 +0200

    Add the method that reads data from the matrix

--- src/clonal_selection/Parser.java
+++ src/clonal_selection/Parser.java
@@ -2,6 +2,7 @@ package clonal_selection;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -66,42 +67,53 @@ public class Parser {
 		//creates both way frequency relations between cabinets and puts them into an arrayList
 		public List<FrequencyRelation> createRelations(List<Integer> numbers, int size) {
 			
+			List<FrequencyRelation> frequencyRelations = new ArrayList<FrequencyRelation>();
+			
 			int lineLength = (int) Math.sqrt(size) - 1;
 			int omitCounter = 0;
+			int counter1 = 0;
+			int counter2 = 0;
 			char leter1 =  98;
 			char leter2 =  97;
 			String letter1 = String.valueOf(leter1);
 			String letter2 = String.valueOf(leter2);
-
-
-			List <FrequencyRelation> frequencyRelations = new ArrayList<FrequencyRelation>();
 			
-			for(Integer frequencyNumber : numbers) {
-				while(numbers.indexOf(frequencyNumber) == omitCounter) {
-					System.out.println("still in the loop");
-					frequencyNumber = numbers.indexOf(frequencyNumber) + 1;
-					continue;
-				}
-				frequencyRelations.add(new FrequencyRelation(letter1, letter2, frequencyNumber));
-				frequencyRelations.add(new FrequencyRelation(letter2, letter1, frequencyNumber));
+			
+			Iterator<Integer> iterator = numbers.iterator();
+			int frequency = iterator.next();
+			
+			while(iterator.hasNext()) {
 				
-				System.out.print( frequencyNumber  + " ");
-				leter1 ++;
+				while(counter1 < lineLength) {
+					frequencyRelations.add(new FrequencyRelation(letter1, letter2, frequency));
+					frequencyRelations.add(new FrequencyRelation(letter2, letter1, frequency));
+					System.out.print(letter1 + letter2 + frequency + " ");
+					counter1++;
+					leter1++;
+					letter1 = String.valueOf(leter1);
+					frequency = iterator.next();
+				}
+				
+				lineLength--;
+				counter1 = 0;
+				leter1 = (char) (98 + 1 + omitCounter);
 				letter1 = String.valueOf(leter1);
-				letter2 = String.valueOf(leter2);
-
-				if(numbers.indexOf(frequencyNumber) == lineLength) {
-					System.out.println("");
-					omitCounter++;
-					leter2++;
-					leter1 = 0;
-					lineLength = lineLength * 2 + 1;
-				}
 				
+				while(counter2 <= omitCounter) {
+					if(!iterator.hasNext()) return frequencyRelations;
+					frequency = iterator.next();
+					counter2++;
+				}
+				omitCounter++;
+				counter2 = 0;
+				leter2++;
+				letter2 = String.valueOf(leter2);
+				
+				System.out.println("");
 			}
 			
 			
-			return new ArrayList<FrequencyRelation>();
+			return frequencyRelations;
 			
 		}
 
